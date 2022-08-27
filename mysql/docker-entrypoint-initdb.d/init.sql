b09e54603fdee86a4e31d6d8ae61fa9204097d9e
kohei23n <kohei.s.n@gmail.com>
2022-08-27 15:37:45 +0900

    add：テーブル情報追加

--- mysql/docker-entrypoint-initdb.d/init.sql
+++ mysql/docker-entrypoint-initdb.d/init.sql
@@ -22,19 +22,124 @@ SET
 
 DROP TABLE IF EXISTS events;
 
-CREATE TABLE events (
+CREATE TABLE airbnbs (
   id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  name VARCHAR(255) NOT NULL,
+  img VARCHAR(255) NOT NULL,
+  location BIT NOT NULL,
+  service BIT NOT NULL,
+  price INT NOT NULL,
+  capacity INT NOT NULL,
+  popularity BIT NOT NULL
 );
 
 INSERT INTO
-  events
+  airbnbs
 SET
-  title = 'イベント1';
+  name = 'おのかんの家',
+  img = 'onokan.jpg',
+  location = 1,
+  service = 0,
+  price = 1000,
+  capacity = 15,
+  popularity = 0;
 
 INSERT INTO
-  events
+  airbnbs
 SET
-  title = 'イベント2';
+  name = 'なおきの家',
+  img = 'naoki.jpg',
+  location = 0,
+  service = 1,
+  price = 10,
+  capacity = 30,
+  popularity = 0;
+
+INSERT INTO
+  airbnbs
+SET
+  name = 'けんとの家',
+  img = 'kento.jpg',
+  location = 1,
+  service = 0,
+  price = 500,
+  capacity = 5,
+  popularity = 0;
+
+INSERT INTO
+  airbnbs
+SET
+  name = 'かずきの家',
+  img = 'kazuki.jpeg',
+  location = 0,
+  service = 0,
+  price = 2000,
+  capacity = 7,
+  popularity = 1;
+
+INSERT INTO
+  airbnbs
+SET
+  name = 'こうへいの家',
+  img = 'kohei.jpg',
+  location = 1,
+  service = 0,
+  price = 10000,
+  capacity = 10,
+  popularity = 1;
+
+INSERT INTO
+  airbnbs
+SET
+  name = 'じんの家',
+  img = 'jin.jpg',
+  location = 1,
+  service = 0,
+  price = 30000,
+  capacity = 8,
+  popularity = 0;
+
+INSERT INTO
+  airbnbs
+SET
+  name = 'まいのの家',
+  img = 'maino.jpg',
+  location = 1,
+  service = 1,
+  price = 100000,
+  capacity = 15,
+  popularity = 1;
+
+INSERT INTO
+  airbnbs
+SET
+  name = 'さちの家',
+  img = 'sachi.jpg',
+  location = 1,
+  service = 0,
+  price = 120000,
+  capacity = 20,
+  popularity = 0;
+
+INSERT INTO
+  airbnbs
+SET
+  name = 'ももの家',
+  img = 'momo.jpg',
+  location = 1,
+  service = 0,
+  price = 500,
+  capacity = 3,
+  popularity = 1;
+
+INSERT INTO
+  airbnbs
+SET
+  name = 'HarborS',
+  img = 'harbors.jpg',
+  location = 1,
+  service = 1,
+  price = 900,
+  capacity = 65,
+  popularity = 0;
+
