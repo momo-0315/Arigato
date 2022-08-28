c404c30a70a14c505f8d8a07d897452f37c6db85
冨永桃 <momotominaga@tominagamomonoMacBook-Pro.local>
2022-08-29 01:05:06 +0900

    headerに検索欄を追加

--- src/user/index.php
+++ src/user/index.php
@@ -1,25 +1,16 @@
 <?php
 session_start();
-require(dirname(__FILE__) . "/dbconnect.php");
+require("../dbconnect.php");
 
 $stmt = $db->query('SELECT * FROM airbnbs');
 $airbnbs = $stmt->fetchAll();
 
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>サンプル</title>
-    <link rel="stylesheet" href="./css/style.css">
-    <link rel="stylesheet" href="./css/normalize.css">
-</head>
+<?php include('./common/user_header.php'); ?>
 <div class="list">
     <?php foreach ($airbnbs as $airbnb) : ?>
         <div class="list__item">
-            <img class="list__item--img" src="./img/airbnbs/<?= $airbnb["img"] ?>" alt="airbnb">
+            <img class="list__item--img" src="../img/airbnbs/<?= $airbnb["img"] ?>" alt="airbnb">
             <h1 class="list__item--title"><?= $airbnb["name"] ?></h1>
             <p class="list__item--price">¥<?= $airbnb["price"] ?>/泊</p>
             <p class="list__item--capacity">収容：<?= $airbnb["capacity"] ?>人</p>
