8699db45f61cf5b8c6ae3b3a5f94e07f89398d84
kohei23n <kohei.s.n@gmail.com>
2022-09-02 12:59:00 +0900

    add：フィルターの見た目完了

--- src/user/index.php
+++ src/user/index.php
@@ -10,6 +10,20 @@ $airbnbs = $stmt->fetchAll();
 <?php include('./common/user_header.php'); ?>
 
 <body>
+    <div class="filter">
+        <div class="filter__item">
+            <img class="filter__item--img" src="../img/filters/support.png"></i>
+            <p class="filter__item--text">サービス</p>
+        </div>
+        <div class="filter__item">
+            <img class="filter__item--img" src="../img/filters/location.png"></i>
+            <p class="filter__item--text">立地良き</p>
+        </div>
+        <div class="filter__item">
+            <img class="filter__item--img" src="../img/filters/fire.png"></i>
+            <p class="filter__item--text">人気</p>
+        </div>
+    </div>
     <div class="list">
         <?php foreach ($airbnbs as $airbnb) : ?>
             <div class="list__item">
