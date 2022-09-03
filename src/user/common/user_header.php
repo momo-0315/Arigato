7de5f16ef0c8e08dcfc110dd39ffa940f5e9066a
kohei23n <kohei.s.n@gmail.com>
2022-09-03 19:33:19 +0900

    add：いらないJS削除。どのフィルターがかかっているかわからないなど修正点必要

--- src/user/common/user_header.php
+++ src/user/common/user_header.php
@@ -12,11 +12,6 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@300;600&display=swap" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- jquery / ajax -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <header class="header">
     <div class="header__logo">
--- src/user/index.php
+++ src/user/index.php
@@ -7,21 +7,23 @@ $airbnbs = $stmt->fetchAll();
 
 // フィルター処理
 if (isset($_POST['filter_service'])) {
-    
-    // フィルター更新
+
+    // サービスフィルター
     $stmt = $db->query('SELECT * FROM airbnbs WHERE service = 1 AND hide = 0');
     $airbnbs = $stmt->fetchAll();
 
 } elseif (isset($_POST['filter_location'])) {
 
-    // フィルター更新
+    // 立地フィルター
     $stmt = $db->query('SELECT * FROM airbnbs WHERE location = 1 AND hide = 0');
     $airbnbs = $stmt->fetchAll();
+
 } elseif (isset($_POST['filter_fire'])) {
 
-    // フィルター更新
+    // 人気フィルター
     $stmt = $db->query('SELECT * FROM airbnbs WHERE popularity = 1 AND hide = 0');
     $airbnbs = $stmt->fetchAll();
+
 }
 
 ?>
@@ -31,15 +33,15 @@ if (isset($_POST['filter_service'])) {
 <body>
     <form action="" method="post">
         <div class="filter">
-            <button class="filter__item" name="filter_service">
+            <button class="filter__item" name="filter_service" id="service">
                 <img class="filter__item--img" src="../img/filters/support.png"></i>
                 <p class="filter__item--text">サービス</p>
             </button>
-            <button class="filter__item" name="filter_location">
+            <button class="filter__item" name="filter_location" id="location">
                 <img class="filter__item--img" src="../img/filters/location.png"></i>
                 <p class="filter__item--text">立地良き</p>
             </button>
-            <button class="filter__item" name="filter_fire">
+            <button class="filter__item" name="filter_fire" id="popularity">
                 <img class="filter__item--img" src="../img/filters/fire.png"></i>
                 <p class="filter__item--text">人気</p>
             </button>
