48473df7b080722f9cf14d8f791c9547fbd925bb
{Y-K-15} <ppscam225@gmail.com>
2022-09-04 14:17:23 +0900

    見た目完成

--- src/admin/login.php
+++ src/admin/login.php
@@ -1,2 +1,16 @@
 <?php include('./common/admin_header.php'); ?>
 
+<section class="login__wrapper">
+
+  <div class="login">
+
+    <h1>管理者ログイン</h1>
+
+    <input type="email" name="email" id="email" placeholder="Email Address">
+    <br>
+    <input type="password" name="password" id="password" placeholder="Password">
+    <br>
+    <input type="submit" name="submit" id="submit" value="ログイン">
+
+  </div>
+</section>
